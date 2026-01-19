b208094f9d3623b067dfd110a5f8827177e457df
Felipe <costadecarvalho.felipe@gmail.com>
2026-01-18 22:18:53 -0300

    quickSort

--- QuickSort.py
+++ QuickSort.py
@@ -47,6 +47,42 @@ def find_max_recursivo_2(array):
 
     return array[0] if array[0] > max_resto else max_resto
 
-arrei = [2, 8, 1, 4, 7, 3, 9, 2, 5, 12, 3]
+def quicksort(array):
+    if len(array) < 2:
+        return array
 
-print(find_max_recursivo_2(arrei))
+    pivot = array[0]
+
+    left = [i for i in array[1:] if i < pivot]
+    right = [i for i in array[1:] if i > pivot]
+    middle = [i for i in array[1:] if i == pivot]
+
+    return quicksort(left) + quicksort(middle) + [pivot] + quicksort(right)
+
+def quick_sort_in_place(array, begin, end):
+    if begin < end:
+        pivot = partition(array, begin, end)
+        quick_sort_in_place(array, begin, pivot - 1)
+        quick_sort_in_place(array, pivot + 1, end)
+
+
+def partition(array, begin, end):
+
+    pivot = array[end]
+
+    i = begin - 1
+
+    for j in range(begin, end):
+        if array[j] <= pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+
+    array[i + 1], array[end] = array[end], array[i + 1]
+
+    return i
+
+arrei = [6, 8, 1, 4, 7, 3, 9, 2, 5, 12, 6]
+
+quick_sort_in_place(arrei, 0, len(arrei) - 1)
+
+print(arrei)
